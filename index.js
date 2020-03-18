a7fad32d73d77a375cb67fc7457c360f5cd05787
soumyadeepsp <soumyadeepsp@gmail.com>
2020-03-19 02:41:41 +0530

    third commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', require('./routes/index'));
+
 app.listen(port, function(err){
     if (err) {
         console.log("Error in runnin gthe server on port : "+port);
